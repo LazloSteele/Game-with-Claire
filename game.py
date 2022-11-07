de71210951f2e3d678d54c1f0c7c68e34c138f14
lsteele2000 <lsteele2000@gmail.com>
2022-11-06 17:53:53 -0800

    initial add of cmdline arg support

--- game.py
+++ game.py
@@ -1,9 +1,14 @@
 from player import Player
 from controller import Controller
 from game_state import GameState
+from game_config import GameConfig
+import argparse
 
 class Game():
     def __init__(self):
+        self._cmdline_args = self.process_cmdline()
+        print( 'Cmdline Args:', self._cmdline_args)
+        print( self._cmdline_args.config_path)
         self._player = Player()
         self._game_state = GameState(self._player)
     
@@ -12,6 +17,30 @@ class Game():
         self._current_room = self._game_state.current_room()
 
         self.main()
+
+    def process_cmdline(self):
+        parser = argparse.ArgumentParser(
+            description='Ways to change the game.',
+            epilog='We await your decision.'
+        )
+        parser.add_argument(
+            '--config_path', 
+            default='.\Config', 
+            metavar='GameConfigFolder',
+            help='Path to the game_name .json config'
+        )
+        parser.add_argument(
+            '--game_name', 
+            default='Default' ,
+            metavar='GameConfig',
+            help='Name of the game, assumed a .json filename'
+        )
+        parser.add_argument(
+            '--player_name', 
+            default='Guest',
+            help='Your name -- pending implementation'
+            )
+        return parser.parse_args()
 
     def main(self):
         playing = True
---
+++ game_config.py
@@ -0,0 +1,12 @@
+
+import json
+
+class GameConfig():
+    def __init__(self):
+        self._file_path = 'Config\\Default.json'
+        print( self._file_path )
+
+    def loadConfig(cfgName='default', path='Config'):
+        pass
+    
+
